5e401875b921ed602bcf166f751e821280e823b3
Hewah Bahrami <hewbahrami@gmail.com>
2024-02-15 18:25:15 +0000

    Update Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ git config --global --add safe.directory /github/workspace
 python3 /usr/bin/feed.py
 
 git add . && git commit -m "Update Feed"
-git push --set-upstream origin/main
+git push --set-upstream origin main
 
 
 echo "========================"
